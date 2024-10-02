c7ed702c044c07aff57df704ff74140de50b1cb8
saidsn <saidsn@code.edu.az>
2024-10-02 12:31:27 +0400

    Lesson Code

---
+++ 01-Operators, DataTypes/script.js
@@ -0,0 +1,110 @@
+// var a = 5;
+// var b = 10
+// var c = a + b;
+// console.log(c);
+
+// let x = 3;
+// let y = 6
+// let z = x * y
+// console.log(z);
+
+// let e = 10;
+
+// let d = 10 * e;
+// console.log(d);
+
+// var a = 155;
+// var b = 155;
+// a = a - b;
+// a -= b;
+
+// a = a * b
+// a *= b;
+
+// a = a % b;
+// a%=b
+
+// a**=b
+// if (a > b) {
+//   console.log("a -boyukdur b-den");
+// } else if (a == b) {
+//   console.log("a - beraberdir b- ile");
+// } else {
+//   console.log("a - kicikdir b- den");
+// }
+
+// console.log(a > b ? "a - boyukdur b -den" : "a - kicikdir b- den" );
+// let name = "Elnur ";
+// let surname = "Elekberov";
+
+// name +=surname;
+// console.log(name);
+
+// let age = 20;
+// let point = 40
+
+// if (age != 22 || point <= 50) {
+//   console.log("Siz sertifikat ala bilersiniz");
+// }
+// else if (age >= 18 && point < 50) {
+//   console.log("Siz pointiniz yeterli deyil");
+// }
+// else {
+//   console.log("Siz sertifikat ala bilmezsiniz");
+// }
+
+// String;
+// Number;
+// Bigint;
+// Boolean;
+// Undefined;
+// Null;
+// Symbol;
+// Object;
+
+// let obj = {
+//   name: "Elnur",
+//   surname: "Elekberov",
+// };
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// function Sum() {
+//   return undefined;
+// }
+
+// let name = "Elnur";
+// console.log(typeof name);
+// let a = 10;
+// console.log(typeof typeof a);
+
+// console.log(typeof Sum());
+
+// let a = 10;
+// let b = "Salam";
+
+// let c = a == b;
+
+// console.log(typeof c);
+
+// let age;
+
+// if (age < 18) {
+//   console.log("siz sexsiyyet vesiqesi ala bilmezsinsiz");
+// } else if (age >= 18 && age <= 57) {
+//   console.log("Siz sexsiyyet vesiqesi ala bilersiniz");
+// } else {
+//   console.log("Allah sizi rehmet elesin");
+// }
+
+let hour = 20;
+
+if (hour > 6 && hour < 12) {
+  console.log("Good morning");
+} else if (hour >= 12 && hour <= 18) {
+  console.log("Good afternoon");
+} else if (hour > 18 && hour <= 24) {
+  console.log("Axsaminiz xeyir");
+} else {
+  console.log("Good evening");
+}
--- DataTypes/script.js
+++
@@ -1,5 +0,0 @@
-let a = 4;
-let b = 10;
-let c = a + b;
-console.log(c);
-
