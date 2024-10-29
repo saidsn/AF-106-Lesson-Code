167bfdef485b2e91c5f31ffbac89ddc8ece27707
saidsn <saidsn@code.edu.az>
2024-10-29 22:52:42 +0400

    Lesson added

---
+++ 15-Js-Intro/assets/js/script.js
@@ -0,0 +1,68 @@
+
+"use strict";
+// console.log("Hello af106");
+// console.log("Sagol af106");
+// console.log("Orxan indi geldi");
+
+// let h1 = document.querySelector("h1");
+// h1.addEventListener("click", () => {
+//   h1.style.color = "red";
+// });
+// console.log(h1);
+
+// let a = 3;
+// let b = 6;
+
+// let c;
+// c = a + b;
+// console.log(c)
+
+
+
+let x = 44;
+// // x = 12;
+console.log(x);
+
+// let y = 10;
+// let y = 77
+// // y = 33;
+// console.log(y);
+
+// const pi = 3.14;
+// // pi = 8;
+// console.log(pi);
+
+// var admin = "Mehemmed"
+
+// if (true) {
+//   var name = "Orxan";
+// }
+// // console.log(age);
+// var name = "Orxan";
+// if (true) {
+//   //   let age = 25;
+//   console.log(name);
+// }
+
+// function say() {
+//     var word = "Hello";
+
+//     console.log(word);
+
+// }
+
+// say()
+console.log(b);
+if (b == undefined) {
+  console.log("b is undefined");
+}
+
+if (true) {
+  var b = 23;
+}
+
+// console.log(a);
+
+// if (true) {
+//   let a = 3;
+// }
